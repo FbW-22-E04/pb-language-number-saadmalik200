da5a8926b54ba3b08b8e5b37eb5965f7c26da4d9
Saad Malik <saad_malik200@hotmail.com>
2022-07-05 14:09:53 +0200

    exercise 1

---
+++ solution.js
@@ -0,0 +1,17 @@
+// ## 1. Concatenation
+
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+
+console.log(introSentence + age);
+// + operator is converting number to string
+
+// ## 2. Converting
+
+const integer = "1005";
+console.log(typeof integer);
+const integer1 = parseInt(integer);
+console.log(typeof integer1, integer1);
+
+const float = "10.05";
+console.log(parseInt(float));
